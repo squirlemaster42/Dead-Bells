cc7fc0d9fa26260b7df1d72c0db8f13c3c3194b6
Jakob Misbach <jakobmisbach8@gmail.com>
2018-06-28 23:17:03 -0400

    Worked on entity

--- Dead Bells/src/com/nerds/entity/Entity.java
+++ Dead Bells/src/com/nerds/entity/Entity.java
@@ -1,20 +1,25 @@
 package com.nerds.entity;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public abstract class Entity {
 
-    private float x, y;
-    private int width, height;
-    private BufferedImage texture;
+    private final float x, y;
+    private final int width, height;
+    private final BufferedImage texture;
 
-    public Entity(float x, float y, int width, int height, BufferedImage texture){
+    private final Rectangle bounds;
+
+    public Entity(final float x, final float y, final int width, final int height, final BufferedImage texture){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.texture = texture;
+
+        bounds = new Rectangle((int) x, (int) y, width, height);
     }
 
     public abstract void tick();
