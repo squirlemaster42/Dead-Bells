2899db0a444376a2de06c2fa57605473668853c5
Jakob Misbach <jakobmisbach8@gmail.com>
2018-07-04 22:19:09 -0400

    Window is too small

--- Dead Bells/src/com/nerds/main/Main.java
+++ Dead Bells/src/com/nerds/main/Main.java
@@ -1,10 +1,12 @@
 package com.nerds.main;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Hi Tre");
-        Game game = new Game();
+        Game game = new Game(new Dimension(1280, 720));
         game.start();
     }
 
