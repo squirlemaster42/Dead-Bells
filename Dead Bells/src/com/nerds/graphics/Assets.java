0cd37124fa33ed86b16c62857be4e03d171bab97
Tre <baroncinitre@gmail.com>
2018-07-05 14:34:59 -0400

    Loaded ground sprite sheet

--- Dead Bells/src/com/nerds/graphics/Assets.java
+++ Dead Bells/src/com/nerds/graphics/Assets.java
@@ -1,8 +1,22 @@
 package com.nerds.graphics;
 
+import java.awt.image.BufferedImage;
+
 public class Assets {
 
-    public static void init(){
 
+    public static BufferedImage tree, dirt, grass, rock, stoneBrick, alien;
+
+    private static final int width = 32, height = 32;
+
+    public static void init(){
+        SpriteSheet ground = new SpriteSheet(ImageLoader.loadImage("/textures/ground.png"));
+
+        grass = ground.crop(0, 0, width, height);
+        dirt = ground.crop(width, 0, width, height);
+        rock = ground.crop(width * 2, 0, width, height);
+        stoneBrick = ground.crop(width * 3, 0, width, height);
+        tree = ground.crop(0, height, width, height);
+        alien = ground.crop(width, height, width, height);
     }
 }
