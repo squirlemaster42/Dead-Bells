897b96272c37e9ad436023ec0fe1ebc392599390
Tre <baroncinitre@gmail.com>
2018-07-05 20:09:04 -0400

    im dumb

--- Dead Bells/src/com/nerds/states/GameState.java
+++ Dead Bells/src/com/nerds/states/GameState.java
@@ -8,23 +8,24 @@ public class GameState extends State {
 
     private final World world;
 
-    public GameState(){
+    public GameState() {
         super("gameState");
         world = new World("TODO");
     }
 
     @Override
-    public void tick(){
+    public void tick() {
         world.tick();
     }
 
     @Override
-    public void render(Graphics g){
+    public void render(Graphics g) {
         world.render(g);
     }
 
     @Override
-    public World getWorld(){
+    public World getWorld() {
         return world;
     }
+}
 
