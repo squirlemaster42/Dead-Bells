f2853c4efc8f68ff5897b01f5f2af88831f468a1
Tre <baroncinitre@gmail.com>
2018-07-05 21:49:06 -0400

    H

--- Dead Bells/src/com/nerds/entity/Character.java
+++ Dead Bells/src/com/nerds/entity/Character.java
@@ -1,22 +1,18 @@
 package com.nerds.entity;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Character extends Creature {
 
-    private static boolean w, a, s, d;
-
     @Override
     public void tick(){
-        w = keys[KeyEvent.VK_W];
-        s = keys[KeyEvent.VK_S];
-        a = keys[KeyEvent.VK_A];
-        d = keys[KeyEvent.VK_D];
+
     }
 
     @Override
-    public void render(){
+    public void render(Graphics g){
 
     }
 }
--- Dead Bells/src/com/nerds/input/KeyManager.java
+++ Dead Bells/src/com/nerds/input/KeyManager.java
@@ -6,13 +6,17 @@ import java.awt.event.KeyListener;
 public class KeyManager implements KeyListener {
 
     private final boolean[] keys;
+    public boolean w, a, s, d;
 
     public KeyManager(){
         keys = new boolean[256];
     }
 
     public void tick(){
-
+        w = keys[KeyEvent.VK_W];
+        s = keys[KeyEvent.VK_S];
+        a = keys[KeyEvent.VK_A];
+        d = keys[KeyEvent.VK_D];
     }
 
     @Override
