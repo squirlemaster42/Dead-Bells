ed3b853689e6cf8ee5a24a59d89d69b6d34aa293
Jakob Misbach <jakobmisbach8@gmail.com>
2018-07-05 21:50:35 -0400

    Fixed constructor

--- Dead Bells/src/com/nerds/entity/Character.java
+++ Dead Bells/src/com/nerds/entity/Character.java
@@ -1,10 +1,17 @@
 package com.nerds.entity;
 
+import com.nerds.utils.Handler;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 
 public class Character extends Creature {
+
+    public Character(Handler handler, float x, float y, int width, int height, BufferedImage img) {
+        super(handler, x, y, width, height, img);
+    }
 
     @Override
     public void tick(){
